c694bb224be01f41bfefbd04fce139e2e58b5fce
Alexander Bezobchuk <abezobchuk@gmail.com>
2016-02-24 23:35:19 -0500

    Style cleanup

--- app/api/better_doctor/v1/base.rb
+++ app/api/better_doctor/v1/base.rb
@@ -1,9 +1,15 @@
 module BetterDoctor
   module V1
     class Base < Grape::API
+
+      # --------------------------------------------------------------------- #
+      #                              Base V1 API                              #
+      # --------------------------------------------------------------------- #
+
+      helpers BetterDoctor::V1::ApiHelpers
       version 'v1', using: :path
 
-      # Mount routes
+      # Mount v1 routes
       mount BetterDoctor::V1::Doctor
     end
   end
