38f237b0cfc17a42976aeae75d0eb1fb68203b0c
Alexander Bezobchuk <abezobchuk@gmail.com>
2016-02-24 23:34:47 -0500

    Style cleanup

--- app/api/better_doctor/base.rb
+++ app/api/better_doctor/base.rb
@@ -1,10 +1,17 @@
 module BetterDoctor
   class Base < Grape::API
+
+    # ----------------------------------------------------------------------- #
+    #                                 Base API                                #
+    # ----------------------------------------------------------------------- #
+
     helpers BetterDoctor::ApiHelpers
     prefix :api
 
     format :json
     content_type :json, 'application/json'
+    # rescue_from :all
+    # error_formatter :json, BetterDoctor::ErrorFormatter
 
     # Version 1 of the API
     mount BetterDoctor::V1::Base
