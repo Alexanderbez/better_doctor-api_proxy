a619b19b81ee782e63b3d5e0ed00e4fcc5121267
Alex Bezobchuk <abezobchuk@jibe.com>
2016-02-29 10:33:15 -0500

    Implement more integration tests

--- spec/requests/api/v1/messages_spec.rb
+++ spec/requests/api/v1/messages_spec.rb
@@ -5,6 +5,10 @@ require 'spec_helper'
 # --------------------------------------------------------------------------- #
 
 describe BetterDoctor::V1 do
+  before(:all) do
+    @api_key = ENV['API_KEY']
+  end
+
   it 'does not accept missing API key HTTP header' do
     # Perform request with missing API key HTTP header
     get '/api/v1/doctors/search'
@@ -17,7 +21,7 @@ describe BetterDoctor::V1 do
     expect(json['response']).to eq('Unauthorized')
   end
 
-  it 'returns proper response on invalid API key HTTP header' do
+  it 'returns proper response for invalid API key HTTP header' do
     # Perform request with missing API key HTTP header
     get '/api/v1/doctors/search', headers: {'X-Api-Key' => 'nil'}
 
@@ -29,5 +33,28 @@ describe BetterDoctor::V1 do
     expect(json['response']['meta']['error']).to eq(true)
     expect(json['response']['meta']['message']).to eq('Invalid user_key')
     expect(json['response']['meta']['error_code']).to eq(1000)
+  end
+
+  it 'returns proper response for valid API key HTTP header' do
+    # Verify API key validity
+    expect(@api_key).to be_truthy
+
+    # Perform request with API key HTTP header
+    get '/api/v1/doctors/search', headers: {'X-Api-Key' => @api_key}
+
+    # Verify correct status code
+    expect(response).to be_success
+  end
+
+  it 'returns proper response for default proxy query' do
+    # Perform request with API key HTTP header
+    get '/api/v1/doctors/search', headers: {'X-Api-Key' => @api_key}
+
+    # Verify correct status code
+    expect(response).to be_success
+
+    # Verify response body
+    expect(json['meta']).not_to be_empty
+    expect(json['data']).not_to be_empty
   end
 end
